217ec8b940136ebb7ad798b3f9c11cc25193fa3b
PopovOleg <jim_power@mail.ru>
2023-04-25 16:30:45 +0300

    .\Task_13\

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -1,5 +1,28 @@
 ﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 //Пример:
-//645 -> 4,
+//645 -> 5,
 //78 -> третьей цифры нет,
 //32679 -> 6.
+
+int rand = new Random().Next(80, 120);
+
+Console.WriteLine("Рамдомное число:  " + rand);
+
+FistNumber(rand);
+Console.WriteLine(" " + rand);
+//Console.WriteLine(" " + rand);
+
+int FistNumber(int x)
+{
+    int a = x % 10;
+    if (a>0)
+     {
+        rand = a;
+     }   
+    else
+    {
+       Console.Write("Третьей цифры нет"); 
+    }
+    return x;
+}
+//Console.WriteLine(" " + rand);
